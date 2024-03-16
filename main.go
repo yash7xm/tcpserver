4e0f4b4b8a68ed126701b14e6e9ee731589d8096
yash7xm <yash.7xm@gmail.com>
2024-03-16 22:54:10 +0530

    read the req and send the response

--- main.go
+++ main.go
@@ -3,7 +3,23 @@ package main
 import (
 	"log"
 	"net"
+	"time"
 )
+
+func do(conn net.Conn) {
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Processing the request")
+	time.Sleep(5 * time.Second)
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
+	conn.Close()
+}
 
 func main() {
 	listener, err := net.Listen("tcp", ":2323")
@@ -17,6 +33,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(conn)
 	log.Println("client connected")
+	do(conn)
 }
